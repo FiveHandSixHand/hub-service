575203b260f731807bd09b5116620db5bf56e901
jihoon <git4ub0@gmail.com>
2026-03-30 16:11:59 +0900

    feat: HubInventoryRepository 추가

---
+++ src/main/java/com/daitda/hubservice/hubinventory/repository/HubInventoryRepository.java
@@ -0,0 +1,21 @@
+package com.daitda.hubservice.hubinventory.repository;
+
+import com.daitda.hubservice.hubinventory.entity.HubInventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface HubInventoryRepository extends JpaRepository<HubInventory, UUID> {
+
+    Optional<HubInventory> findByHubInventoryIdAndDeletedAtIsNull(UUID hubInventoryId);
+
+    Optional<HubInventory> findByHubIdAndCompanyIdAndProductIdAndDeletedAtIsNull(UUID hubId, UUID companyId, UUID productId);
+
+    List<HubInventory> findAllByHubIdAndDeletedAtIsNull(UUID hubId);
+
+    List<HubInventory> findAllByDeletedAtIsNull();
+
+    boolean existsByHubIdAndCompanyIdAndProductIdAndDeletedAtIsNull(UUID hubId, UUID companyId, UUID productId);
+}
