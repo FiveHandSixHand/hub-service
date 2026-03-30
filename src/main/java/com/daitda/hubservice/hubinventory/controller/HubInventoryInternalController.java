6e30d69334cead0f5a3167c5e00e2c3009d2718d
jihoon <git4ub0@gmail.com>
2026-03-31 04:02:48 +0900

    feat: HubInventoryInternalController 추가

---
+++ src/main/java/com/daitda/hubservice/hubinventory/controller/HubInventoryInternalController.java
@@ -0,0 +1,51 @@
+package com.daitda.hubservice.hubinventory.controller;
+
+import com.daitda.hubservice.hubinventory.dto.command.DecreaseHubInventoryCommand;
+import com.daitda.hubservice.hubinventory.dto.command.RestoreHubInventoryCommand;
+import com.daitda.hubservice.hubinventory.dto.request.DecreaseHubInventoryRequest;
+import com.daitda.hubservice.hubinventory.dto.request.RestoreHubInventoryRequest;
+import com.daitda.hubservice.hubinventory.dto.response.FindHubInventoryResponse;
+import com.daitda.hubservice.hubinventory.service.HubInventoryService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/internal/v1/hub-inventories")
+public class HubInventoryInternalController {
+
+    private final HubInventoryService hubInventoryService;
+
+    public HubInventoryInternalController(HubInventoryService hubInventoryService) {
+        this.hubInventoryService = hubInventoryService;
+    }
+
+    // 재고 차감
+    @PatchMapping("/decrease")
+    public ResponseEntity<FindHubInventoryResponse> decreaseHubInventory(@Valid @RequestBody DecreaseHubInventoryRequest request) {
+        DecreaseHubInventoryCommand command = DecreaseHubInventoryCommand.builder()
+                .hubInventoryId(request.getHubInventoryId())
+                .quantity(request.getQuantity())
+                .build();
+
+        FindHubInventoryResponse response = hubInventoryService.decreaseHubInventory(command, null);
+
+        return ResponseEntity.ok(response);
+    }
+
+    // 재고 복원
+    @PatchMapping("/restore")
+    public ResponseEntity<FindHubInventoryResponse> restoreHubInventory(@Valid @RequestBody RestoreHubInventoryRequest request) {
+        RestoreHubInventoryCommand command = RestoreHubInventoryCommand.builder()
+                .hubInventoryId(request.getHubInventoryID())
+                .quantity(request.getQuantity())
+                .build();
+
+        FindHubInventoryResponse response = hubInventoryService.restoreHubInventory(command, null);
+
+        return ResponseEntity.ok(response);
+    }
+}
