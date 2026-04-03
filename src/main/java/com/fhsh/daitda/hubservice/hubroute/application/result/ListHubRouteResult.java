eac9949cad88e0bdca7cc147da971b13ce4b9e45
jihoon <git4ub0@gmail.com>
2026-04-03 23:46:14 +0900

    feat: HubRoute list result 추가

---
+++ src/main/java/com/fhsh/daitda/hubservice/hubroute/application/result/ListHubRouteResult.java
@@ -0,0 +1,29 @@
+package com.fhsh.daitda.hubservice.hubroute.application.result;
+
+import com.fhsh.daitda.hubservice.hubroute.doamin.entity.HubRoute;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ListHubRouteResult(
+        UUID hubRouteId,
+        UUID srcHubId,
+        UUID destHubId,
+        Integer durationTime,
+        BigDecimal distance,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+    public static ListHubRouteResult from(HubRoute hubRoute) {
+        return new ListHubRouteResult(
+                hubRoute.getHubRouteId(),
+                hubRoute.getSrcHubId(),
+                hubRoute.getDestHubId(),
+                hubRoute.getDurationTime(),
+                hubRoute.getDistance(),
+                hubRoute.getCreatedAt(),
+                hubRoute.getUpdatedAt()
+        );
+    }
+}
