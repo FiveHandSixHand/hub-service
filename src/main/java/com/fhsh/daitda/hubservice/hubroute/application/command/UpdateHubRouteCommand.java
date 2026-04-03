a22c37d793998c071fae1d2fafcea2d0566b9b68
jihoon <git4ub0@gmail.com>
2026-04-03 23:38:05 +0900

    feat: HubRoute update command 추가

---
+++ src/main/java/com/fhsh/daitda/hubservice/hubroute/application/command/UpdateHubRouteCommand.java
@@ -0,0 +1,16 @@
+package com.fhsh.daitda.hubservice.hubroute.application.command;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Builder
+public class UpdateHubRouteCommand {
+
+    private UUID hubRouteId;
+    private Integer durationTime;
+    private BigDecimal distance;
+}
