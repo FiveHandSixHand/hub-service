11e3be0a2f6aa72c4e88e012fae51455cc329962
jihoon <git4ub0@gmail.com>
2026-04-03 13:04:12 +0900

    refactor: hub-service 테스트용 설정 분리

--- src/test/java/com/fhsh/daitda/hubservice/HubServiceApplicationTests.java
+++ src/test/java/com/fhsh/daitda/hubservice/HubServiceApplicationTests.java
@@ -3,14 +3,7 @@ package com.fhsh.daitda.hubservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-        properties = {
-                "spring.config.name=application-test",
-                "spring.cloud.config.enabled=false",
-                "spring.cloud.config.import-check.enabled=false",
-                "eureka.client.enabled=false"
-        }
-)
+@SpringBootTest
 class HubServiceApplicationTests {
 
     @Test
