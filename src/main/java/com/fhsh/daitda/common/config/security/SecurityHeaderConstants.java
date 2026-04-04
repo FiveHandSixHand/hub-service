79ed09c23fad2a5fd56527f6c8e96b23af8e9ee9
jihoon <git4ub0@gmail.com>
2026-04-04 21:19:59 +0900

    refactor: security header constants 유틸 클래스 정리

--- src/main/java/com/fhsh/daitda/common/config/security/SecurityHeaderConstants.java
+++ src/main/java/com/fhsh/daitda/common/config/security/SecurityHeaderConstants.java
@@ -1,12 +1,12 @@
 package com.fhsh.daitda.common.config.security;
 
-public class SecurityHeaderConstants {
+public final class SecurityHeaderConstants {
 
     public static final String USER_ID = "X-User-Id";
     public static final String USER_EMAIL = "X-User-Email";
     public static final String USER_ROLE = "X-User-Role";
 
     private SecurityHeaderConstants() {
-        throw new IllegalArgumentException("Utility class");
+        throw new IllegalStateException("Utility class");
     }
 }
