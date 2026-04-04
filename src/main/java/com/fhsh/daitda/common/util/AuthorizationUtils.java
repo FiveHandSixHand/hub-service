9f851437be93b6cd5ef248a8d2c57a408cca18e1
jihoon <git4ub0@gmail.com>
2026-04-04 20:04:43 +0900

    feat: authorization utils 추가

---
+++ src/main/java/com/fhsh/daitda/common/util/AuthorizationUtils.java
@@ -0,0 +1,21 @@
+package com.fhsh.daitda.common.util;
+
+import com.fhsh.daitda.common.exception.ForbiddenException;
+import com.fhsh.daitda.common.model.AuthenticatedUser;
+
+public final class AuthorizationUtils {
+
+    private AuthorizationUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static void validateAdminOrHubAdmin(AuthenticatedUser authenticatedUser) {
+        if (authenticatedUser == null) {
+            throw new ForbiddenException("권한 정보가 존재하지 않습니다.");
+        }
+
+        if (!(authenticatedUser.isAdmin() || authenticatedUser.isHubAdmin())) {
+            throw new ForbiddenException("해당 요청에 대한 권한이 없습니다.");
+        }
+    }
+}
