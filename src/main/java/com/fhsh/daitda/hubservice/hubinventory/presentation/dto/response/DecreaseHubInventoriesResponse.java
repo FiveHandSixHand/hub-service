d38ef01c79e9c8a91425ad42ad5eb622273215e3
jihoon <git4ub0@gmail.com>
2026-04-06 05:18:14 +0900

    feat: DecreaseHubInventoriesResponse bulk decrease response dto 추가

---
+++ src/main/java/com/fhsh/daitda/hubservice/hubinventory/presentation/dto/response/DecreaseHubInventoriesResponse.java
@@ -0,0 +1,39 @@
+package com.fhsh.daitda.hubservice.hubinventory.presentation.dto.response;
+
+import com.fhsh.daitda.hubservice.hubinventory.application.result.FindHubInventoryResult;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+public class DecreaseHubInventoriesResponse {
+
+    private List<Item> items;
+
+    public static DecreaseHubInventoriesResponse from(List<FindHubInventoryResult> results) {
+        return DecreaseHubInventoriesResponse.builder()
+                .items(results.stream()
+                        .map(findHubInventoryResult -> Item.builder()
+                                .hubInventoryId(findHubInventoryResult.hubInventoryId())
+                                .hubInventoryId(findHubInventoryResult.hubId())
+                                .companyId(findHubInventoryResult.companyId())
+                                .productId(findHubInventoryResult.productId())
+                                .quantity(findHubInventoryResult.quantity())
+                                .build())
+                        .toList())
+                .build();
+    }
+
+    @Getter
+    @Builder
+    public static class Item{
+        private UUID hubInventoryId;
+        private UUID hubId;
+        private UUID companyId;
+        private UUID productId;
+        private Integer quantity;
+    }
+}
