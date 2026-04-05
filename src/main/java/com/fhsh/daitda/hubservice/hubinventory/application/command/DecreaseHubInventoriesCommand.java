fc8875c3f70036c5682075e5966e2ef978c8a05c
jihoon <git4ub0@gmail.com>
2026-04-06 05:07:17 +0900

    feat:HubInventory bulk decrease command 추가

---
+++ src/main/java/com/fhsh/daitda/hubservice/hubinventory/application/command/DecreaseHubInventoriesCommand.java
@@ -0,0 +1,21 @@
+package com.fhsh.daitda.hubservice.hubinventory.application.command;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+public class DecreaseHubInventoriesCommand {
+
+    private List<Item> items;
+
+    @Getter
+    @Builder
+    private static class Item{
+        private UUID hubInventoryId;
+        private Integer quantity;
+    }
+}
