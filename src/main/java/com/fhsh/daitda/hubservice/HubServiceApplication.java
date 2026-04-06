4c8ce4853331b3143cd2c636c31245ab3ccd8ffb
jihoon <git4ub0@gmail.com>
2026-04-07 03:19:56 +0900

    refactor: 메인 애플리케이션에서 네이버 설정 바인딩 제거

--- src/main/java/com/fhsh/daitda/hubservice/HubServiceApplication.java
+++ src/main/java/com/fhsh/daitda/hubservice/HubServiceApplication.java
@@ -1,9 +1,7 @@
 package com.fhsh.daitda.hubservice;
 
-import com.fhsh.daitda.hubservice.infrastructure.naver.config.NaverMapProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableConfigurationProperties(NaverMapProperties.class)
 public class HubServiceApplication {
 
     public static void main(String[] args) {
