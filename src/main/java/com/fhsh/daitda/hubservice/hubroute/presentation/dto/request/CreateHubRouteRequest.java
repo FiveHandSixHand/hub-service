757aada347f548bb6d481b199973db74c38fb792
jihoon <git4ub0@gmail.com>
2026-04-06 15:15:41 +0900

    refactor: 불필요한 import 정리

--- src/main/java/com/fhsh/daitda/hubservice/hubroute/presentation/dto/request/CreateHubRouteRequest.java
+++ src/main/java/com/fhsh/daitda/hubservice/hubroute/presentation/dto/request/CreateHubRouteRequest.java
@@ -2,11 +2,9 @@ package com.fhsh.daitda.hubservice.hubroute.presentation.dto.request;
 
 import com.fhsh.daitda.hubservice.hubroute.application.command.CreateHubRouteCommand;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
 @Getter
