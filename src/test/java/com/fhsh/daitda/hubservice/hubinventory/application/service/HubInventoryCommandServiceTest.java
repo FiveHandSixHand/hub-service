29baad73465b8a8643d0773149393daf2f32a4da
jihoon <git4ub0@gmail.com>
2026-04-06 10:59:39 +0900

    test: command service 테스트 클래스명 정리

--- src/test/java/com/fhsh/daitda/hubservice/hubinventory/application/service/HubInventoryCommandServiceTest.java
+++ src/test/java/com/fhsh/daitda/hubservice/hubinventory/application/service/HubInventoryCommandServiceTest.java
@@ -28,7 +28,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class HubInventoryServiceTest {
+public class HubInventoryCommandServiceTest {
 
     @Mock
     private HubInventoryRepository hubInventoryRepository;
