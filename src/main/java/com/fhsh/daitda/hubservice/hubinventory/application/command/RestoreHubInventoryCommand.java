5d47e177800901e9163af8918b2ada327383e7d3
jihoon <git4ub0@gmail.com>
2026-04-06 10:23:28 +0900

    feat: restoration command bulk 구조로 변경

--- src/main/java/com/fhsh/daitda/hubservice/hubinventory/application/command/RestoreHubInventoryCommand.java
+++ src/main/java/com/fhsh/daitda/hubservice/hubinventory/application/command/RestoreHubInventoryCommand.java
@@ -3,12 +3,21 @@ package com.fhsh.daitda.hubservice.hubinventory.application.command;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Builder
 public class RestoreHubInventoryCommand {
 
-    private UUID hubInventoryId;
-    private Integer quantity;
+    // 복원 대상 목록
+    private List<Item> orderItems;
+
+    // 개별 복원 항목
+    @Getter
+    @Builder
+    public static class Item {
+        private UUID hubInventoryId;
+        private Integer quantity;
+    }
 }
